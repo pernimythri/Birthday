466c0ea4d11a63b36bf2e84f60e5a88abea7e81c
pernimythri <157490327+pernimythri@users.noreply.github.com>
2025-02-09 11:08:10 +0530

    Update newscript.js

--- newscript.js
+++ newscript.js
@@ -1,4 +1,3 @@
-// Define the quotes for each image
 const quotes = {
     1: "Yentii na face meedha click chesthav...Friend ani vadhilesthunna",
     2: "Vachadayya !! Yem ledhu I am Innocent ani chepdhamani 😎",
@@ -7,15 +6,12 @@ const quotes = {
     5: "Don't do drama to get things from others ..Be yourself",
     6: "Be funnier, even when I am angry and foolish 😜",
     7: "Last one ... A small Story .. I came through this message from my family group..Everyone likes butterfly but not caterpillar, at one stage u will also become butterfly wait Karoh for that moment..Dont panic and dont stress in your life  <3 .. E quote ayitey mandhey -> Dont judge people by their looks and actions like caterpillar they will also become butterflies in their lives :)",
-    8: "Okati cheppali ..I lied in my 3-1 My grade is not 7.98 ..I got 8.61 .. Happy birthday Dear Birthday Partner"
     };
 
-// Get all the images and the quote container
 const images = document.querySelectorAll(".quote-image");
 const quoteContainer = document.getElementById("quoteContainer");
 const quoteText = document.getElementById("quoteText");
 
-// Add click event listeners to each image
 images.forEach(image => {
     image.addEventListener("click", function() {
         // Get the quote number from the data-quote attribute
@@ -42,19 +38,15 @@ images.forEach(image => {
 function createCrackers() {
     const crackerContainer = document.querySelector('.cracker-container');
     
-    for (let i = 0; i < 100; i++) { // Generate 100 cracker elements
+    for (let i = 0; i < 100; i++) {
         let cracker = document.createElement('div');
         cracker.classList.add('cracker');
-        // Random horizontal and vertical position for crackers
         cracker.style.left = `${Math.random() * 100}%`; 
         cracker.style.top = `${Math.random() * 100}%`; 
-        // Random animation duration for each cracker
         cracker.style.animationDuration = `${Math.random() * 2 + 2}s`;
         
-        // Append the cracker to the container
         crackerContainer.appendChild(cracker);
     }
 }
 
-// Trigger cracker effect on page load
 window.onload = createCrackers;
